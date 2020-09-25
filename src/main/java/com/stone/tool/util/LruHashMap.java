0bd177e7cdf5e3b8d5ea86564c4460fa085c0c53
yuanxiu <qishi.zhou@ideacome.com>
2020-09-25 14:34:18 +0800

    学习手写lru 缓存

--- src/main/java/com/stone/tool/util/LruHashMap.java
+++ src/main/java/com/stone/tool/util/LruHashMap.java
@@ -1,8 +1,28 @@
 package com.stone.tool.util;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * @author yuanxiu
  * @date 2020/9/25
+ *
+ * lru 淘汰算法
+ * 核心LinkedHashMap
  */
-public class LruHashMap {
+public class LruHashMap<K, V> extends LinkedHashMap<K, V> {
+
+    private static final long serialVersionUID = 1L;
+    private final int maxCacheSize;
+
+    LruHashMap(int initialCapacity, int maxCacheSize) {
+        super(initialCapacity, 0.75F, true);
+        this.maxCacheSize = maxCacheSize;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return this.size() > this.maxCacheSize;
+    }
+
 }
